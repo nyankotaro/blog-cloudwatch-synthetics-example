5af3dfb78af9070636dd2126c55cca34b28ad123
nyankotaro <kotaro.takayama0148@gmail.com>
2025-02-02 16:59:38 +0900

    First Commit

---
+++ test/simple-cloudwatch-synthetics.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleCloudwatchSynthetics from '../lib/simple-cloudwatch-synthetics-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple-cloudwatch-synthetics-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleCloudwatchSynthetics.SimpleCloudwatchSyntheticsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
